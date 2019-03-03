1d8f540ba0c68475fd5fa3dfb98976812156c7ed
ChosunOne <koreanhalo@gmail.com>
2019-03-04 00:26:49 +0900

    update example

--- examples/main.rs
+++ examples/main.rs
@@ -6,7 +6,13 @@ fn main() {
     let key = vec![0x00];
     let value = vec![0x00];
 
+    // Inserting and getting from a tree
     let new_root = tree.insert(None, &mut [&key], &mut vec![&value]).unwrap();
     let items = tree.get(&new_root, &mut [&key]).unwrap();
     assert_eq!(items, vec![Some(value)]);
+
+    // Attempting to get from a removed root will yield None
+    tree.remove(&new_root).unwrap();
+    let items2 = tree.get(&new_root, &mut [&key]).unwrap();
+    assert_eq!(items2, vec![None]);
 }
